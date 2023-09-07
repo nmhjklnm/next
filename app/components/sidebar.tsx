f4b7cf318bb8d654130bd26541a33369b0f16dab
Yang YiHe <108562510+nmhjklnm@users.noreply.github.com>
2023-09-07 23:35:21 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -121,7 +121,7 @@ export function SideBar(props: { className?: string }) {
           Chiru
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Elegance in survival's dance
+            Elegance in survival dance
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
